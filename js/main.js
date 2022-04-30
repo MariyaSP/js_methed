d9e3913e466f0c71311eea2ab91298dfd44c1efb
Mariya Kartseva <kjojoj@rambler.ru>
2022-04-30 23:41:58 +0300

    js start

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const productName = "Апельсины";
+const productCount = 5;
+const productCategory = "Фрукты";
+const price = 120;
+
+console.log(productName);
+console.log(`Общая сумма: ${productCount*price}`);
