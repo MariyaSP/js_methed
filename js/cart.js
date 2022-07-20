38695e625d7ee92eb90de5326d327d3b6a7ddb0c
Kartseva Mariya <kartsevaSP@yandex.ru>
2022-07-20 16:49:37 +0300

    add this

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,14 @@
 'use strict';
 const cart = {
   items: [],
+  discount: 0,
   get totalPrice() {
     return this.calculateItemPrice();
+  },
+  set setDiscount(promocode) {
+    if(promocode === "METHED") this.discount = 15;
+    if(promocode === "NEWYEAR") this.discount = 21;
+
   },
   count: 0,
   add(itemName, itemPrice, itemCount = 1) {
@@ -20,7 +26,7 @@ const cart = {
       total += this.items[key].count * this.items[key].price;
       this.increaseCount(this.items[key].count);
     }
-    return total;
+    return (total - (total * (this.discount / 100)));
   },
   clear() {
     this.items = [];
@@ -35,4 +41,5 @@ cart.add('велосипед', 23000, 9);
 cart.add('самокат', 15000, 5);
 cart.add('скейт', 5000, 2);
 cart.add('электросамокат', 150000, 1);
+cart.setDiscount = "NEWYEAR";
 cart.print();
